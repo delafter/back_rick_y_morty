649674b94c93b1ffd2483000797cb51e34a45673
Manu <delafter@gmail.com>
2024-07-07 21:33:32 +0200

    jsonwebtoken funcionando y asignando token

--- models/user.js
+++ models/user.js
@@ -3,30 +3,29 @@ const mongoose = require("mongoose");
 //crear un nuevo usuario
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String,
-        required: true
-    },
+  name: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-    age: {
-        type: Number,
-        required: false
-    },
+  age: {
+    type: Number,
+    required: false,
+  },
 
-    password: {
-        type: String,
-        required: true
-    }
-}); 
+  password: {
+    type: String,
+    required: true,
+  },
+
+  
+});
 
 //obtener un usuario por su id
-
-
-
-
 
 module.exports = mongoose.model("User", userSchema);
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,55 +1,83 @@
 const express = require("express");
-
-const userSchema =require("../models/user");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const userSchema = require("../models/user");
 
 const router = express.Router();
 
-    
-
 //crear un nuevo usuario
 
-router.post("/usuarios", (req, res) => {
-    const user = userSchema(req.body);
-    user.save()
-        .then((data) => res.json({message: "Usuario creado", data: data}))
-        .catch((error) => res.json({ message: error.message }));
-        
-}); 
+router.post("/usuarios/signup", (req, res) => {
+  const user = userSchema(req.body);
+  user
+    .save()
+    .then((data) => res.json({ message: "Usuario creado", data: data }))
+    .catch((error) => res.json({ message: error.message }));
+});
+
+// loguear un usuario
+
+router.post("/usuarios/login", (req, res) => {
+  userSchema.findOne({ email: req.body.email, password: req.body.password })
+    .then((data) => {
+      if (data) {
+        const userForToken = {
+          email: data.email,
+          id: data._id,
+        };
+        const token = jwt.sign({ userForToken }, process.env.JWT_SECRET_KEY, {
+          expiresIn: "1h",
+        });
+        res.json({ message: "Usuario logueado", token, data: data});
+      } else {
+        res.json({ message: "Usuario no encontrado" }), 404;
+      }
+    })
+    .catch((error) =>{
+      console.log(token)
+       res.json({ message: error.message })}), 500;
+});
 
 //obtener unsuario por su id
 
-router.get('/usuarios/:id', (req, res) => {
-    userSchema.findById(req.params.id)
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error.message }));
+router.post("/usuarios/login/:id", (req, res) => {
+  userSchema
+    .findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        res.json({ message: "Usuario no encontrado" }), 404;
+      } else {
+        res.json(data), 200;
+      }
+    })
+    .catch((error) => res.json({ message: error.message }));
 });
 
 //obtener todos los usuarios
 
-router.get('/usuarios', (req, res) => {
-    userSchema.find()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error.message }));
+router.get("/usuarios", (req, res) => {
+  userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }));
 });
 
 //actualizar un usuario por su id
 
-router.patch('/usuarios/:id', (req, res) => {
-    userSchema.findByIdAndUpdate(req
-        .params.id, req.body, {new: true})
-        .then((data) => res.json({message: "Usuario modificado", data: data}))
-        .catch((error) => res.json({ message: error.message }));
+router.patch("/usuarios/:id", (req, res) => {
+  userSchema
+    .findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((data) => res.json({ message: "Usuario modificado", data: data }))
+    .catch((error) => res.json({ message: error.message }));
 });
 
 //eliminar un usuario por su id
 
-router.delete('/usuarios/:id', (req, res) => {
-    userSchema.findByIdAndDelete(req.params.id)
-        .then((data) => res.json({message: "Usuario eliminado"}))
-        .catch((error) => res.json({ message: error.message }));
+router.delete("/usuarios/:id", (req, res) => {
+  userSchema
+    .findByIdAndDelete(req.params.id)
+    .then(() => res.json({ message: "Usuario eliminado" })) //no se pone data porque no hay nada que devolver
+    .catch((error) => res.json({ message: error.message }));
 });
-
-
-
 
 module.exports = router;
